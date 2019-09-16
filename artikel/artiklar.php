f66de3fcb4552c5e269ac302a14426cfc78eaa83
daande01 <daniel.evert.andersson@gmail.com>
2019-09-16 09:41:12 +0200

    update

--- artikel/artiklar.php
+++ artikel/artiklar.php
@@ -1,3 +1,16 @@
+<?php
+
+$art=array(
+      array("lg 32",1000,"tv1.","bild.png"),
+      array("lg 42",2000,"tv2.","bild.png"),
+      array("lg 48",3000,"tv3","bild.png"),
+
+);
+
+
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -7,27 +20,24 @@
   </head>
   <body>
 <?php
-$artikelNamn="lg 32";
-$pris=1999;
-$beskrivning="mycket kompetent tv med full HD. En av de bästa i världen.";
-$bild="bild.png";
-echo"<id=\"dfddfghhg\">";
 
+echo"<div id=\"wrapper\">";
 
-
+for ($row=0; $row <3 ; $row++) {
 echo <<<ARTIKEL
 
 <div id="artikel">
 <fieldset id="field">
-<legend> {$artikelNamn}   </legend>
-
-<img src="bilder/{$bild}" alt="lg 32" >
-pris: {$pris} kr
+<legend> {$art[$row][2]}   </legend>
+<img src="bilder/{$art[$row][3]}" alt="{$art[$row][0]}" >
+pris: {$art[$row][1]} kr
 </fieldset>
 </div>
 ARTIKEL;
+}
+
 
  ?>
-
+</div>
 </body>
 </html>
