13f81be855cebe426b72d5a70472d1a85771c651
daande01 <daniel.evert.andersson@gmail.com>
2020-01-27 11:02:53 +0100

    new files

--- mvctemplate/postmodel.php
+++ mvctemplate/postmodel.php
@@ -3,7 +3,7 @@
 class postmodel extends DB {
 
   function get(){
-    return $this->select("SELECT * FROM `posts`");
+    return $this->select("SELECT * FROM `posts` where post_pk= :num", array(':num' => 2));
   }
 }
 ?>
---
+++ mvctemplate/postmodelexempel.php
@@ -0,0 +1,36 @@
+// model
+
+
+<?php
+class postmodel extends DB {
+  function get(){
+    return $this->select("SELECT * FROM `posts`");
+  }
+}
+?>
+
+_________________________________________
+
+
+<?php
+function postview(){
+  ?>
+  <div class="row">
+    <div class="col-xs-12 col-md-8">
+      <ul><?php
+        $postmodel = new postmodel();
+        foreach ($postmodel->get() as $post) {
+          echo "<li>" . $post['rubrik'] . "</li>";
+        }
+      ?></ul>
+    </div>
+    <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>  
+  </div>
+  
+  <?php
+  
+}
+
+//////_________________________________________
+
+
